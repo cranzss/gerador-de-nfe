e4472516f1bd3ab875a604af185d46babc9898c1
chris <chris@DESKTOP-6E9ETN2>
2024-03-04 12:47:26 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace geradordenf
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            resources.GetString("comboBox1.Items"),
+            resources.GetString("comboBox1.Items1"),
+            resources.GetString("comboBox1.Items2"),
+            resources.GetString("comboBox1.Items3"),
+            resources.GetString("comboBox1.Items4"),
+            resources.GetString("comboBox1.Items5"),
+            resources.GetString("comboBox1.Items6"),
+            resources.GetString("comboBox1.Items7"),
+            resources.GetString("comboBox1.Items8"),
+            resources.GetString("comboBox1.Items9"),
+            resources.GetString("comboBox1.Items10"),
+            resources.GetString("comboBox1.Items11"),
+            resources.GetString("comboBox1.Items12"),
+            resources.GetString("comboBox1.Items13"),
+            resources.GetString("comboBox1.Items14"),
+            resources.GetString("comboBox1.Items15"),
+            resources.GetString("comboBox1.Items16"),
+            resources.GetString("comboBox1.Items17"),
+            resources.GetString("comboBox1.Items18"),
+            resources.GetString("comboBox1.Items19"),
+            resources.GetString("comboBox1.Items20"),
+            resources.GetString("comboBox1.Items21"),
+            resources.GetString("comboBox1.Items22"),
+            resources.GetString("comboBox1.Items23"),
+            resources.GetString("comboBox1.Items24"),
+            resources.GetString("comboBox1.Items25"),
+            resources.GetString("comboBox1.Items26")});
+            resources.ApplyResources(this.comboBox1, "comboBox1");
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace geradordenf
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        string GerarCNPJ()
+        {
+            // Gerar 8 números aleatórios de 0 a 9 e 4 números (0001), e depois calcular eles
+            // Colocar eles na maskedTextBox
+
+            Random rnd = new Random();
+            int n1 = rnd.Next(0, 10);
+            int n2 = rnd.Next(0, 10);
+            int n3 = rnd.Next(0, 10);
+            int n4 = rnd.Next(0, 10);
+            int n5 = rnd.Next(0, 10);
+            int n6 = rnd.Next(0, 10);
+            int n7 = rnd.Next(0, 10);
+            int n8 = rnd.Next(0, 10);
+            int n9 = 0;
+            int n10 = 0;
+            int n11 = 0;
+            int n12 = 1;
+
+            int Soma1 = n1 * 5 + n2 * 4 + n3 * 3 + n4 * 2 + n5 * 9 + n6 * 8 + n7 * 7 + n8 * 6 + n9 * 5 + n10 * 4 + n11 * 3 + n12 * 2;
+
+            int DV1 = Soma1 % 11;
+
+            if (DV1 < 2)
+            {
+                DV1 = 0;
+            }
+            else
+            {
+                DV1 = 11 - DV1;
+            }
+
+            int Soma2 = n1 * 6 + n2 * 5 + n3 * 4 + n4 * 3 + n5 * 2 + n6 * 9 + n7 * 8 + n8 * 7 + n9 * 6 + n10 * 5 + n11 * 4 + n12 * 3 + DV1 * 2;
+
+            int DV2 = Soma2 % 11;
+
+            if (DV2 < 2)
+            {
+                DV2 = 0;
+            }
+            else
+            {
+                DV2 = 11 - DV2;
+            }
+
+            return n1.ToString() + n2 + n3 + n4 + n5 + n6 + n7 + n8 + n9 + n10 + n11 + n12 + DV1 + DV2;
+        }
+        static string SerieNF(Random rnd, int minLength)
+        {
+            const string chars = "0123456789";
+            char[] result = new char[minLength];
+
+            // Gere caracteres aleatórios até alcançar o tamanho mínimo especificado
+            for (int i = 0; i < minLength; i++)
+            {
+                result[i] = chars[rnd.Next(chars.Length)];
+            }
+
+            return new string(result);
+        }
+        static string NumeroNF(Random rand, int minLength)
+        {
+            const string chars = "0123456789";
+            char[] result = new char[minLength];
+
+            // Gere caracteres aleatórios até alcançar o tamanho mínimo especificado
+            for (int i = 0; i < minLength; i++)
+            {
+                result[i] = chars[rand.Next(chars.Length)];
+            }
+
+            return new string(result);
+        }
+
+        static string CodNF(Random rando, int minLength)
+        {
+            const string chars = "0123456789";
+            char[] result = new char[minLength];
+
+            // Gere caracteres aleatórios até alcançar o tamanho mínimo especificado
+            for (int i = 0; i < minLength; i++)
+            {
+                result[i] = chars[rando.Next(chars.Length)];
+            }
+
+            return new string(result);
+        }
+
+        static string CodUF(string uf)
+        {
+            switch (uf)
+            {
+                case "AC": return "12";
+                case "AL": return "27";
+                case "AP": return "16";
+                case "AM": return "13";
+                case "BA": return "29";
+                case "CE": return "23";
+                case "DF": return "53";
+                case "ES": return "32";
+                case "GO": return "52";
+                case "MA": return "21";
+                case "MT": return "51";
+                case "MS": return "50";
+                case "MG": return "31";
+                case "PA": return "15";
+                case "PB": return "25";
+                case "PR": return "41";
+                case "PE": return "26";
+                case "PI": return "22";
+                case "RJ": return "33";
+                case "RN": return "24";
+                case "RS": return "43";
+                case "RO": return "11";
+                case "RR": return "14";
+                case "SC": return "42";
+                case "SP": return "35";
+                case "SE": return "28";
+                case "TO": return "17";
+            }
+
+            return uf;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //mês e ano da nota
+            string Data = DateTime.Now.ToString("yy''MM");
+
+            if (comboBox1.SelectedIndex == -1)
+            {
+                MessageBox.Show("Por favor, selecione uma UF válida.");
+                return;
+            }
+
+            string uf = comboBox1.SelectedItem.ToString();
+            string ufcod = CodUF(uf);
+            //string coduf = ObterCodigoUF(uf);
+
+            //modelo da nota (55 para NF-e)
+            int mod = 55;
+            string mode = mod.ToString();
+
+            //série da nf
+            Random rnd = new Random(DateTime.Now.Millisecond);
+            string ser = SerieNF(rnd, 3);
+
+            //número da nf
+            Random rand = new Random(DateTime.Now.Millisecond);
+            string num = NumeroNF(rand, 9);
+
+            //código numérico da nf
+            Random rando = new Random(DateTime.Now.Millisecond);
+            string cod = CodNF(rand, 8);
+
+            //chave nf com 43 digitos
+            string chave = ufcod + Data + GerarCNPJ() + mode + ser + num + "1" + cod;
+
+            // cálculo do dígito verificador da NF
+            int peso = 2;
+            int soma = 0;
+
+            for (int i = chave.Length - 1; i >= 0; i--)
+            {
+                int digito = int.Parse(chave[i].ToString());
+                soma += digito * peso;
+
+                peso++;
+                if (peso > 9)
+                    peso = 2;
+            }
+
+            int resto = soma % 11;
+            int digitoVerificador = 11 - resto;
+            if (digitoVerificador > 9)
+                digitoVerificador = 0;
+
+            string digit = digitoVerificador.ToString();
+
+            string chavenfe = chave + digit;
+            int tamanhochave = chavenfe.Length;
+
+            Console.WriteLine(ser +" "+ num + " " + cod + " tamanho chave:" + tamanhochave);
+            textBox1.Text = chavenfe;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
